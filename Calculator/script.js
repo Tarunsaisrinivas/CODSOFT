c58c7ef491eeb1a20e8a6e0eb50d8ef68ca955b8
Tarun Sai Srinivas <92567478+Tarunsaisrinivas@users.noreply.github.com>
2023-07-13 09:41:53 +0530

    Add files via upload

---
+++ Calculator/script.js
@@ -0,0 +1,77 @@
+// script.js
+// script.js
+document.addEventListener("DOMContentLoaded", function() {
+  const input = document.getElementById("input");
+  const buttons = document.querySelectorAll(".calculator button");
+
+  buttons.forEach(button => {
+    button.addEventListener("click", function() {
+      const buttonText = this.textContent;
+      handleInput(buttonText);
+    });
+  });
+
+  document.addEventListener("keydown", function(event) {
+    const key = event.key;
+    if (key.match(/[0-9+\-*/.%]/)) {
+      handleInput(key);
+    } else if (key === "Backspace") {
+      handleDelete();
+    } else if (key === "Enter" || key === "=") {
+      evaluateExpression();
+    }
+  });
+
+  function handleInput(value) {
+    if (value === "AC") {
+      input.value = "";
+    } else if (value === "DEL") {
+      handleDelete();
+    } else if (value === "=") {
+      evaluateExpression();
+    } else {
+      input.value += value;
+    }
+  }
+
+  function handleDelete() {
+    input.value = input.value.slice(0, -1);
+  }
+
+  function evaluateExpression() {
+    try {
+      input.value = eval(input.value);
+    } catch (error) {
+      input.value = "Error";
+    }
+  }
+});
+
+  
+        
+        // // script.js
+        // document.addEventListener("DOMContentLoaded", function() {
+        //     const input = document.getElementById("input");
+        //     const buttons = document.querySelectorAll(".calculator button");
+            
+        //     buttons.forEach(button => {
+        //       button.addEventListener("click", function() {
+        //         const buttonText = this.textContent;
+                
+        //         if (buttonText === "AC") {
+        //           input.value = "";
+        //         } else if (buttonText === "DEL") {
+        //           input.value = input.value.slice(0, -1);
+        //         } else if (buttonText === "=") {
+        //           try {
+        //             input.value = eval(input.value);
+        //           } catch (error) {
+        //             input.value = "Error";
+        //           }
+        //         } else {
+        //           input.value += buttonText;
+        //         }
+        //       });
+        //     });
+        //   });
+          
